c5a1d4ac0a0465d058b089c79cb8b378b7dd4608
Danil Ineev <saigono@users.noreply.github.com>
2019-10-14 21:59:35 +0100

    Update 78.subsets.py

--- 78.subsets.py
+++ 78.subsets.py
@@ -1,16 +1,10 @@
 # https://leetcode.com/problems/subsets/
-# ugly solution
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = [[]]
-        level = set([tuple()])
-        for i in range(len(nums)):
-            new_level = set()
-            for s in level:
-                for n in nums:
-                    if n not in s:
-                        new_level.add(tuple(sorted(s + (n, ))))
-            level = new_level
-            res += [list(x) for x in level]
+        
+        while nums:
+            n = nums.pop(0)
+            res += [x + [n] for x in res]
         return res
