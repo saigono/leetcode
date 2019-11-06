2810b1a735b20738890c4cb2d1d2d47ee45bfea2
Danil Ineev <saigono@users.noreply.github.com>
2019-11-06 23:16:00 +0000

    Create 100.same_tree.py

---
+++ 100.same_tree.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/same-tree/
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
