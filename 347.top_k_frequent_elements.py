63cbc16222df5b013d480d6283ec693f104e9871
Danil Ineev <saigono@users.noreply.github.com>
2019-11-06 22:23:08 +0000

    Create 347.top_k_frequent_elements.py

---
+++ 347.top_k_frequent_elements.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/top-k-frequent-elements
+
+from collections import Counter
+from itertools import chain
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        frequencies = Counter(nums)
+        
+        inv_freq = {}
+        for num, freq in frequencies.items():
+            s = inv_freq.setdefault(freq, set())
+            s.add(num)
+        
+        top_freq = chain(*[inv_freq[key] for key in sorted(inv_freq, reverse=True)])
+        result = []
+        for _ in range(k):
+            result.append(next(top_freq))
+        return result
