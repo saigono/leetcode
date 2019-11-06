3da2cdde4185a2fb7adfcb98ebf835e4b8bc611a
Danil Ineev <saigono@users.noreply.github.com>
2019-11-06 22:38:06 +0000

    Create 937.reorder_data_in_log_files.py

---
+++ 937.reorder_data_in_log_files.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/reorder-data-in-log-files/
+
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        digit_logs = []
+        letter_logs = []
+        for l in logs:
+            tag, line = l.split(' ', 1)
+            if line[0].isdigit():
+                digit_logs.append(l)
+            else:
+                letter_logs.append((line, tag))
+        
+        letter_logs.sort()
+        result = []
+        for line, tag in letter_logs:
+            result.append(f'{tag} {line}')
+        result += digit_logs
+        return result
+ 
